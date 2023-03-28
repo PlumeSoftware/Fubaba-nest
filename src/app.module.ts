894377cde0ab69ae2c55a48fe149d93ea23396c6
unknown <corfer@yeah.net>
2023-03-28 20:22:10 +0800

    config: pnpm & reducing mailer

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,23 +3,28 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { meta } from '../lib/entity/meta';
 import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from '../auth/guard/auth.guard';
-import { LogService } from '../ops/log/log.service';
-import { MailService } from '../ops/mail/mail.service';
+// import { AuthGuard } from '../auth/guard/auth.guard';
+// import { LogService } from '../ops/log/log.service';
+// import { MailService } from '../ops/mail/mail.service';
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get('SQL_HOST'),
-        port: configService.get('SQL_PORT'),
-        username: configService.get('SQL_USER'),
-        password: configService.get('SQL_PASS'),
-        database: configService.get('SQL_BASE'),
-        entities: meta,
-        synchronize: true,
+        type: 'mssql',
+        host: 'localhost',
+        port: 1433,
+        username: 'sa',
+        password: 'fbb&654321',
+        database: 'fbb',
+        // entities: meta,
+        options: {
+          encrypt: false,
+          trustServerCertificate: true,
+        },
+        synchronize: false,
+
       }),
       inject: [ConfigService],
     }),
@@ -29,12 +34,12 @@ import { MailService } from '../ops/mail/mail.service';
   ],
   providers: [
     //接口鉴权、日志管理和邮件功能需要全局使用
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard,
-    },
-    LogService,
-    MailService
+    // {
+    //   provide: APP_GUARD,
+    //   useClass: AuthGuard,
+    // },
+    // LogService,
+    // MailService
   ],
 })
 export class AppModule { }
