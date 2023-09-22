64e0b1da0dbd220fb7ae7653b24eaa228b82e654
unknown <corfer@yeah.net>
2023-09-22 14:11:13 +0800

    refactor: 合并配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { entities as enDl } from '../lib/entity/metaDl';
-import { entities as enZh } from '../lib/entity/metaZh';
+import { entities } from '../lib/entity/entites';
 import { APP_GUARD } from '@nestjs/core';
 import { AgentModule } from '../app/agent/agent.module';
 import { FyModule } from '../app/fy/fy.module';
 import { UserModule } from '../app/user/user.module';
-import { FubabaUser } from '../lib/entity/meta_dl/user';
+import { FubabaUser } from '../lib/entity/common/user';
 // import { AuthGuard } from '../auth/guard/auth.guard';
 // import { LogService } from '../ops/log/log.service';
 // import { MailService } from '../ops/mail/mail.service';
@@ -20,7 +19,7 @@ const configService = new ConfigService();
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule, AgentModule, FyModule],
-      name: "fmj",
+      name: configService.get('SQL_DB_DL'),
       useFactory: () => ({
         type: 'mssql',
         host: configService.get('SQL_HOST'),
@@ -28,7 +27,7 @@ const configService = new ConfigService();
         username: configService.get("SQL_USER_DL"),
         password: configService.get('SQL_PASS_DL'),
         database: configService.get('SQL_DB_DL'),
-        entities: enDl,
+        entities: entities,
         options: {
           encrypt: false,
           trustServerCertificate: true,
@@ -39,30 +38,6 @@ const configService = new ConfigService();
       }),
       inject: [ConfigService],
     }),
-    TypeOrmModule.forFeature(enDl, "fmj"),
-
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule, AgentModule, FyModule],
-      name: "zh_erp",
-      useFactory: () => ({
-        type: 'mssql',
-        host: configService.get('SQL_HOST'),
-        port: Number(configService.get('SQL_PORT')),
-        username: configService.get("SQL_USER_ZH"),
-        password: configService.get('SQL_PASS_ZH'),
-        database: configService.get('SQL_DB_ZH'),
-        entities: enZh,
-        options: {
-          encrypt: false,
-          trustServerCertificate: true,
-        },
-        // @ts-ignore
-        poolSize: 10,
-        synchronize: false,
-      }),
-      inject: [ConfigService],
-    }),
-    TypeOrmModule.forFeature(enZh, "zh_erp"),
 
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule, UserModule],
@@ -86,7 +61,6 @@ const configService = new ConfigService();
       }),
       inject: [ConfigService],
     }),
-    TypeOrmModule.forFeature([FubabaUser], "fbb_cp"),
   ],
   providers: [
     //接口鉴权、日志管理和邮件功能需要全局使用
